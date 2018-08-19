77b5efd9a3dbc25f399b6c8a713fa4d33b9b262a
Eraldo Paes <eraldopaesbarreto1984@gmail.com>
2018-08-19 03:37:10 -0300

    melhoria no adapter

--- app/src/main/java/odlare/com/pontinho/PlayerListAdapter.java
+++ app/src/main/java/odlare/com/pontinho/PlayerListAdapter.java
@@ -1,17 +1,21 @@
 package odlare.com.pontinho;
 
-
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
 public class PlayerListAdapter extends  RecyclerView.Adapter<PlayerListAdapter.PlayerViewHolder>{
+
+    private static final String S3_BASE_URL = "https://s3.us-east-2.amazonaws.com/segurofacil/pontinho/";
 
     private Context context;
     private List<Player> players;
@@ -33,7 +37,10 @@ public class PlayerListAdapter extends  RecyclerView.Adapter<PlayerListAdapter.P
 
         if (!players.isEmpty()) {
             Player player = players.get(position);
-            holder.name.setText(player.getName());
+            holder.name.setText(String.format("%s %s", player.getName(), player.getLastname()));
+            holder.email.setText(player.getEmail());
+            holder.phone.setText(player.getPhone());
+            Glide.with(holder.itemView).load(S3_BASE_URL + player.getName().toLowerCase() + ".jpg").into(holder.photo);
         }
     }
 
@@ -53,11 +60,17 @@ public class PlayerListAdapter extends  RecyclerView.Adapter<PlayerListAdapter.P
 
     public class PlayerViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView name;
+        private TextView name;
+        private TextView phone;
+        private TextView email;
+        private ImageView photo;
 
         public PlayerViewHolder(View itemView) {
             super(itemView);
             this.name = itemView.findViewById(R.id.player_name);
+            this.phone = itemView.findViewById(R.id.player_phone);
+            this.email = itemView.findViewById(R.id.player_email);
+            this.photo = itemView.findViewById(R.id.player_photo);
         }
     }
 }
